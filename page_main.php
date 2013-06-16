42a0c51505e53c9faa891c252f46f332195adbfe
Joakim Hedlund <contact@joakimhedlund.com>
2013-06-16 15:09:00 +0200

    Moved inline styles to master sheet

--- page_main.php
+++ page_main.php
@@ -14,13 +14,13 @@
 	<script src="js/otworlds.init.js"></script>
 </head>
 <body id="page-main">
-	<div class="toolbar" id="menu" style="width: 600px; position: fixed; top: 20px; left: 300px; background-color: rgba(255, 255, 255, 0.9); border: 1px solid rgba(0, 0, 0, 0.3); border-radius: 4px; padding: 1em;">
+	<div class="toolbar" id="menu" style="width: 600px; top: 20px; left: 300px;">
 		Here go some Options
 	</div>
-	<div class="toolbar" id="meta" style="width: 200px; position: fixed; right: 20px; top: 200px; background-color: rgba(255, 255, 255, 0.9); border: 1px solid rgba(0, 0, 0, 0.3); border-radius: 4px; padding: 1em;">
+	<div class="toolbar" id="meta" style="width: 200px; right: 20px; top: 200px;">
 		Toggle edit mode.
 	</div>
-	<div class="toolbar" id="brushes" style="width: 200px; position: fixed; left: 20px; top: 200px; background-color: rgba(255, 255, 255, 0.9); border: 1px solid rgba(0, 0, 0, 0.3); border-radius: 4px; padding: 1em;">
+	<div class="toolbar" id="brushes" style="width: 200px; left: 20px; top: 200px;">
 		Brushes be crazy!
 	</div>
 	<div style="position: fixed; width: 39%; left: 30%; top: 200px; background-color: rgba(255, 220, 255, 0.9); border: 1px solid rgba(0, 0, 0, 0.3); border-radius: 4px; padding: 1em;">
@@ -44,7 +44,7 @@
 			</div>
 		</div>
 	</div>
-	<div id="canvas" style="height: 100%; overflow: hidden; background-image: url(img/transparent.png);">
+	<div id="canvas">
 		&nbsp;
 	</div>
 </body>
