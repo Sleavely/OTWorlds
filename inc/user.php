929a25a9eb31169a27d3be9a67f72b81e61589ae
Joakim Hedlund <contact@joakimhedlund.com>
2013-06-17 18:47:36 +0200

    Handle OAuthExceptions (such as expired sessions)

--- inc/user.php
+++ inc/user.php
@@ -1,11 +1,16 @@
 <?php
 
-require_once('facebook-php-sdk/facebook.php');
-
-$facebook = new Facebook(array(
-  'appId'  => '391055467680449',
-  'secret' => '828dd5806edaba916664ced4c3edd830',
-));
-
-// Get User ID
-$user = $facebook->getUser();
+try{
+	require_once('facebook-php-sdk/facebook.php');
+	
+	$facebook = new Facebook(array(
+	  'appId'  => '391055467680449',
+	  'secret' => '828dd5806edaba916664ced4c3edd830',
+	));
+	
+	// Get User ID
+	$user = $facebook->getUser();
+	
+}catch(Exception $e){
+	$user = 0;
+}
