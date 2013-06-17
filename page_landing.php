02b9a3f5a7979b679e776f6890a6bb016daf230a
Joakim Hedlund <contact@joakimhedlund.com>
2013-06-17 08:02:17 +0200

    Removed mobile viewport meta. Looked horrible on phone!

--- page_landing.php
+++ page_landing.php
@@ -6,7 +6,6 @@
 	<title>OTWorlds Mapeditor</title>
 	
 	<link rel="shortcut icon" href="img/spellbook-16x16.png" type="image/x-icon" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1" />
 	<link rel="stylesheet" href="css/style.css" />
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
 	
