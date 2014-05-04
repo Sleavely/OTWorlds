c489f8b758725c788981aa0297fdf39048681500
Joakim Hedlund <github.com@cip.nu>
2014-05-04 16:32:46 +0200

    jquery-infinite-drag 11bb636

--- js/jquery.infinitedrag/jquery.infinitedrag.js
+++ js/jquery.infinitedrag/jquery.infinitedrag.js
@@ -59,15 +59,21 @@
 		var $draggable = $(draggable);
 		var $viewport = $draggable.parent();
 		$draggable.css({
-			position: "relative",
-			cursor: "move"
+			position: "relative"
 		});
 
 		// Draggable options
 		var _do = {
-			shouldEase: false
+			shouldEase: false,
+			cursor: true
 		};
 		$.extend(_do, draggable_options);
+
+		if (_do.cursor) {
+			$draggable.css({
+				cursor: "move"
+			});
+		}
 
 		// Tile options (DEFAULT)
 		var _to = {
@@ -286,9 +292,11 @@
 
 			$draggable.draggable("option", "disabled", value);
 
-			$draggable.css({
-				cursor: (value) ? "default" : "move"
-			});
+			if (_do.cursor) {
+				$draggable.css({
+					cursor: (value) ? "default" : "move"
+				});
+			}
 		};
 
 		self.center = function(col, row) {
