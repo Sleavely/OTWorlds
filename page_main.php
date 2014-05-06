567f697dc034f4e48f097393408e6d7286f8c2ae
Joakim Hedlund <contact@joakimhedlund.com>
2014-05-06 13:31:13 +0200

    Removed menu and meta toolbars

--- page_main.php
+++ page_main.php
@@ -45,14 +45,6 @@
 	<div id="viewport">
 		<div id="canvas"></div>
 	</div>
-	<div class="toolbar" id="menu" style="width: 600px; top: 20px; left: 400px; opacity: 0;">
-		<ul>
-			<li>File</li><li>Edit</li><li>Map</li><li>Options</li><li>Help</li>
-		</ul>
-	</div>
-	<div class="toolbar" id="meta" style="width: 200px; right: 20px; top: 200px; opacity: 0;">
-		Meta tools
-	</div>
 	<div class="toolbar" id="brushes" style="width: 250px; left: 0px; top: 0px; bottom: 0px; opacity: 0; overflow-y: scroll;">
 		<ul id="itemlist">
 		</ul>
