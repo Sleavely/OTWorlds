a393da5a9355fbad953d602a2d985718acf8ff2f
Joakim Hedlund <contact@joakimhedlund.com>
2014-12-07 15:17:03 +0100

    Updated deployment procedure to properly deal with composer.lock

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+# Make sure we're on the right branch
 git checkout master
+# Reset the views that we minified last deployment
 git checkout -- app/views/
+# Now download the latest and greatest
 git pull --no-edit origin master
-composer dump-autoload
-php artisan migrate
+
+# Update Composer itself and then any dependencies from composer.lock
+composer self-update
+composer install
+
+# Run any user-defined upgrades
+php artisan migrate --force
+
+# Minify and Uglify
 grunt build
